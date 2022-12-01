188f2121af06959bbdcb943302cbcdded558235a
Javier C <javiercolladolatour@gmail.com>
2022-12-01 12:31:53 +0100

    Paint gen 1 & 2 pokemon

--- myscripts.js
+++ myscripts.js
@@ -2,6 +2,13 @@ fetch("https://pokeapi.co/api/v2/generation/1/")
     .then(response => response.json())
     .then(data => {
         let pokemonList = document.getElementById("pokemon-list")
-        data.moves.forEach(pokemon => { pokemonList.innerHTML += `<li>${pokemon.name}</li>`          
+        data.pokemon_species.forEach(pokemon => { pokemonList.innerHTML += `<li>${pokemon.name}</li>`          
+        });         
+    });
+fetch("https://pokeapi.co/api/v2/generation/2/")
+    .then(response => response.json())
+    .then(data => {
+        let pokemonList = document.getElementById("pokemon-list")
+        data.pokemon_species.forEach(pokemon => { pokemonList.innerHTML += `<li>${pokemon.name}</li>`          
         });         
     });
